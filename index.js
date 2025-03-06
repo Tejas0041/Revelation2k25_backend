8581c01a6e79c85b6a97b0440c4fcb583c0f55b2
Tejas0041 <tejaspawar70238@gmail.com>
2025-03-07 03:23:21 +0530

    Updated CORS origin

--- index.js
+++ index.js
@@ -31,10 +31,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
-    origin: "http://localhost:5173", 
+    // origin: "http://localhost:5173", 
+    origin: "https://revelation2k25.netlify.app/",
     credentials: true 
 }));
 app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    return res.redirect('/admin/login');
+});
 
 // Routes
 app.use('/admin', require('./routes/adminRoutes'));
