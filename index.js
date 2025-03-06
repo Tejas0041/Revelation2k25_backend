3b59d1b920f3311ce737f5ddb59e4a06abef77db
Tejas0041 <tejaspawar70238@gmail.com>
2025-03-07 03:46:21 +0530

    Fixed CORS

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ 
     // origin: "http://localhost:5173", 
-    origin: "https://revelation2k25.netlify.app/",
+    origin: "https://revelation2k25-frontend-testing.vercel.app",
     credentials: true 
 }));
 app.use(methodOverride('_method'));
